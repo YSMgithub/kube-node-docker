cd8a24ccf7b5ea7b1b1c3f5b2e94ab31e7ce94de
Alayna <signalia54@gmail.com>
2023-12-03 00:28:28 +0700

    Dockerfile changed

--- node/my-app.js
+++ node/my-app.js
@@ -3,6 +3,6 @@ http.createServer(function(request,response){
      
     response.end("Hello NodeJS!");
      
-}).listen(3000, "127.0.0.1",function(){
+}).listen(3000, function(){
     console.log("Сервер начал прослушивание запросов на порту 3000");
 });
